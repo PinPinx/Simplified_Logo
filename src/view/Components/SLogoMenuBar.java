6825d5abd9d3880d38670d9c433cdd1d8a11cbd7
Lien <kimlienhg@gmail.com>
2015-02-21 15:26:34 -0500

    added menu bar

---
+++ src/view/Components/SLogoMenuBar.java
@@ -0,0 +1,101 @@
+package view.Components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import view.View;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+
+/**
+ * MenuBar for the SLogo program.
+ * 
+ * @author lien
+ *
+ */
+public class SLogoMenuBar extends MenuBar {
+	
+	// temporarily hardcoded to English - will be drawn from reference libraries 
+	// once locales are set up
+	
+	private View myView;
+	
+	// items under File
+	public static final String FILE = "File";
+	public static final String SAVE_WORKSPACE = "Save workspace";
+	public static final String LOAD_WORKSPACe = "Load workspace";
+	
+	// items under Preferences
+	public static final String PREFERENCES = "Preferences";
+	public static final String BACKGROUND = "Background Color";
+	public static final String TURTLE_IMAGE = "Turtle Image";
+	public static final String PEN_COLOR = "Pen Color";
+	public static final String LANGUAGE = "Language";
+	
+	// items under Edit
+	public static final String EDIT = "Edit";
+	public static final String UNDO = "Undo";
+	public static final String REDO = "Redo";
+	
+	public static final String HELP = "Help";
+	
+	public SLogoMenuBar(View view) {
+		
+		super();
+		myView = view;
+		
+		addMenuList(PREFERENCES, makePreferencesMenu());
+		addMenuList(FILE, makePreferencesMenu());
+		addMenuList(EDIT, makePreferencesMenu());
+		addMenuList(HELP, makePreferencesMenu());
+	}
+	
+	
+	private MenuItem makeMenuItem(String label, EventHandler<ActionEvent> event) {
+		MenuItem item = new MenuItem(label);
+		item.setOnAction(event);
+		return item;
+	}
+	
+	
+	private void addMenuList(String label, List<MenuItem> itemsList) {
+		Menu menuList = new Menu(label);
+		menuList.getItems().addAll(itemsList);
+		getMenus().add(menuList);
+	}
+	
+	
+	private List<MenuItem> makePreferencesMenu() {
+		ArrayList<MenuItem> preferences = new ArrayList<MenuItem>();
+		//MenuItem myBackground  = makeMenuItem(BACKGROUND, event -> myView.changeBackgroundColor());
+		//MenuItem myTurtleImage = makeMenuItem(TURTLE_IMAGE, event -> );
+		//MenuItem myPenColor	 = makeMenuItem(PEN_COLOR, event -> );
+		//MenuItem myLanguage 	 = makeMenuItem(LANGUAGE, event -> );
+		//preferences.addAll(myBackground, myTurtleImage, myPenColor, myLanguage);
+		return preferences;
+	}
+	
+	
+	private List<MenuItem> makeFileMenu() {
+		ArrayList<MenuItem> file = new ArrayList<MenuItem>();
+		//MenuItem mySave = makeMenuItem(SAVE_WORKSPACE, event -> );
+		//MenuItem myLoad = makeMenuItem(LOAD_WORKSPACE, event -> );
+		//file.addAll(mySave, myLoad);
+		return file;
+	}
+	
+	
+	private List<MenuItem> makeEditMenu() {
+		ArrayList<MenuItem> edit = new ArrayList<MenuItem>();
+		//MenuItem myUndo = makeMenuItem(UNDO, event -> );
+		//MenuItem myRedo = makeMenuItem(REDO, event -> );
+		//file.addAll(myUndo, myRedo);
+		return edit;
+	}
+	
+	
+}
