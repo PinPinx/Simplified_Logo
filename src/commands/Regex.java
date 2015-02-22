af1b7926d5145bc8dec1281834751104a1702528
Kaighn Kevlin <kaighn.kevlin@duke.edu>
2015-02-21 22:50:33 -0500

    added Regex and GeneralType enum

---
+++ src/commands/Regex.java
@@ -0,0 +1,71 @@
+package commands;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+public class Regex {
+	String languageFileString;
+	List<Entry<String, Pattern>> generalPatterns = new ArrayList<>();
+	List<Entry<String, Pattern>> commandPatterns = new ArrayList<>();
+
+	public Regex(){
+		languageFileString = "resources/languages/English";
+		commandPatterns.addAll(makePatterns(languageFileString));
+		generalPatterns.addAll(makePatterns("resources/languages/Syntax"));
+	}
+	
+	private boolean match (String input, Pattern regex) {
+		return regex.matcher(input).matches();
+	}
+
+	public GeneralType getType(String s){
+		for (Entry<String, Pattern> p : this.generalPatterns) {
+			if (match(s, p.getValue())) {
+				return GeneralType.findType(p.toString());
+			}
+		}
+		return GeneralType.OTHER;
+	}
+
+	private void testMatches (String[] tests, List<Entry<String, Pattern>> patterns) {
+		for (String s : tests) {
+			boolean matched = false;
+			if (s.trim().length() > 0) {
+				for (Entry<String, Pattern> p : patterns) {
+					if (match(s, p.getValue())) {
+						System.out.println(String.format("%s matches %s", s, p.getKey()));
+						matched = true;
+						break;
+					}
+				}
+				if (! matched) {
+					System.out.println(String.format("%s not matched", s));
+				}
+			}
+		}
+		System.out.println();
+	}
+
+	private List<Entry<String, Pattern>> makePatterns (String syntax) {
+		ResourceBundle resources = ResourceBundle.getBundle(syntax);
+		List<Entry<String, Pattern>> patterns = new ArrayList<>();
+		Enumeration<String> iter = resources.getKeys();
+		while (iter.hasMoreElements()) {
+			String key = iter.nextElement();
+			String regex = resources.getString(key);
+			patterns.add(new SimpleEntry<String, Pattern>(key,
+					Pattern.compile(regex, Pattern.CASE_INSENSITIVE)));
+		}
+		return patterns;
+	}
+}
+
