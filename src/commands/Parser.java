f53227e1e835b233938d792551061c11dfa8ce30
Kaighn Kevlin <kaighn.kevlin@duke.edu>
2015-02-22 04:18:36 -0500

    started work on parser, but interrupted so danny could work

--- src/commands/Parser.java
+++ src/commands/Parser.java
@@ -1,17 +1,29 @@
 package commands;
 
+import java.util.Collections;
+import java.util.Stack;
+
+import Exceptions.CommandNameNotFoundException;
 import model.State;
 
 public class Parser {
 	private State myState;
+	private Regex myRegex;
 	
 	public Parser(State s){
 		this.myState = s;
+		this.myRegex = new Regex();
 	}
 	
-	public CommandRoot parse(String command){
-		//TODO
+	public CommandRoot parse(String command) throws CommandNameNotFoundException{
+		String[] commandStream = command.split("\\p{Space}");
 		CommandRoot root = new CommandRoot(command);
+		Stack<String> commandStringStack = new Stack<>();
+		for(int i = commandStream.length-1; i >= 0; i--){
+			commandStringStack.push(commandStream[i]);
+		}
+		
+		parsePart(commandStringStack, 0);
 		
 		Command testCommand;
 		if(command.length()>5){
@@ -22,5 +34,22 @@ public class Parser {
 		}
 		root.addCommand(testCommand);
 		return root;
+	}
+	
+	public int parsePart(Stack<String> stack, int index) throws CommandNameNotFoundException{
+		int numArgs;
+		Class<?> cls;
+		try {
+			cls = Class.forName("Forward");
+			numArgs = cls.getConstructors()[0].getParameterCount() - 1;
+		} catch (ClassNotFoundException e) {
+			throw new CommandNameNotFoundException();
+		}	
+		
+		if(numArgs == 0){
+			return 0;
+		}
+		
+		return parsePart(commandStream, index + 1);
 	}
 }
